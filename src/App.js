420f9e105a7bd333ddef99ac1913e42f55b21d8a
Henrique <henriquelira99@gmail.com>
2019-12-08 15:30:48 -0300

    Add hooks examples

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-    return <h1>Ola ET</h1>;
+    const [techList, setTechList] = useState([]);
+    const [newTech, setNewTech] = useState('');
+
+    const handleAdd = useCallback(() => {
+        setTechList([...techList, newTech]);
+        setNewTech('');
+    }, [newTech, techList]);
+
+    useEffect(() => {
+        const storageTechList = localStorage.getItem('techList');
+
+        if (storageTechList) {
+            setTechList(JSON.parse(storageTechList));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('techList', JSON.stringify(techList));
+    }, [techList]);
+
+    const techSize = useMemo(() => techList.length, [techList]);
+
+    return (
+        <>
+            <ul>
+                {techList.map(tech => (
+                    <li key={tech}>{tech}</li>
+                ))}
+            </ul>
+            <strong>Você tem {techSize} tecnologias</strong>
+            <br />
+            <input
+                type="text"
+                value={newTech}
+                onChange={e => setNewTech(e.target.value)}
+            />
+            <button type="submit" onClick={handleAdd}>
+                Adicionar
+            </button>
+        </>
+    );
 }
 
 export default App;
